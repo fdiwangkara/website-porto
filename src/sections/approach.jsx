dc4e2bf58f760bca501753f14fce574590abeaa6
fdiwangkara <fdiwangkara@gmail.com>
2024-10-21 08:54:58 +0700

    nambahin animasi dikit

--- src/sections/approach.jsx
+++ src/sections/approach.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import PersonPic from "../images/person.png";
 import Button from "../components/button";
 
 const Approach = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000, // Animation duration in milliseconds
+      once: true, // Ensure animation happens only once while scrolling
+    });
+  }, []);
+
   return (
     <section
       id="approach"
       className="relative h-screen flex flex-col-reverse md:flex-row items-center justify-center"
     >
       {/* Javanese text and welcome message */}
-      <div className="w-full md:w-2/3 md:mr-4 text-center md:text-left">
+      <div
+        className="w-full md:w-2/3 md:mr-4 text-center md:text-left"
+        data-aos="fade-up"
+      >
         <p className="font-secondaryBold text-[40px] md:text-[64px] text-[#3B3030]">
           ꧋ꦱꦸꦒꦼꦁꦫꦮꦸꦃ꧉
         </p>
@@ -25,13 +37,16 @@ const Approach = () => {
           in the tech world.
         </p>
         {/* Button Component */}
-        <div className="pt-7">
+        <div className="pt-7" data-aos="fade-up" data-aos-delay="200">
           <Button text="Explore" targetSection="projects" />
         </div>
       </div>
 
       {/* Image on the right for larger screens, top for mobile */}
-      <div className="w-full md:w-1/3 flex justify-center md:justify-end mb-6 md:mb-0">
+      <div
+        className="w-full md:w-1/3 flex justify-center md:justify-end mb-6 md:mb-0"
+        data-aos="flip-left"
+      >
         <img src={PersonPic} alt="Person" className="h-auto w-2/3 md:w-auto" />
       </div>
     </section>
